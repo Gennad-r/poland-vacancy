0e7561cf00e42712b13f23481a1237b1f2046f88
Gennad-r <gennad.r@gmail.com>
2018-01-15 20:51:08 +0200

    main page

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,29 @@
 $(function() {
+});
 
 	// Custom JS
 	$(".slider-01").owlCarousel({
 		items: 1,
+		autoplay: true,
+		autoplayHoverPause: true,
+		autoplayTimeout: 5000
+	});
+	$(".slider-02").owlCarousel({
+		items: 1,
+		autoplay: true,
+		autoplayHoverPause: true,
+		autoplayTimeout: 3000
+	});
+	$(".slider-03").owlCarousel({
+		items: 1,
+		autoplay: true,
+		autoplayHoverPause: true,
+		autoplayTimeout: 7000
 	});
 
-});
+	$(".accordion-item").on("click", function () {
+		$(".accordion-item").removeClass("selected-item");
+		$(this).toggleClass("selected-item");
+		
+	})
+
